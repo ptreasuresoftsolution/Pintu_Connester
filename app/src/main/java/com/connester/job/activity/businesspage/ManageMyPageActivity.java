ff4e5570b683e06390a768e5352f48c2fa93da6b
imac <pintu033dholariya@gmail.com>
2024-03-11 12:30:19 +0530

    business-page

--- app/src/main/java/com/connester/job/activity/businesspage/ManageMyPageActivity.java
+++ app/src/main/java/com/connester/job/activity/businesspage/ManageMyPageActivity.java
@@ -1,14 +1,35 @@
 package com.connester.job.activity.businesspage;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import com.connester.job.R;
+import com.connester.job.RetrofitConnection.ApiClient;
+import com.connester.job.RetrofitConnection.ApiInterface;
+import com.connester.job.function.SessionPref;
+import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ManageMyPageActivity extends AppCompatActivity {
     String business_page_id = null;
+    Context context;
+    Activity activity;
+    SessionPref sessionPref;
+    MaterialCardView back_cv;
+    ViewPager view_pager;
+    TabLayout tab_layout;
+    List<Fragment> fragments = new ArrayList<>();
+    List<String> fragmentsTitle = new ArrayList<>();
+    ApiInterface apiInterface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +40,22 @@ public class ManageMyPageActivity extends AppCompatActivity {
             business_page_id = getIntent().getStringExtra("business_page_id");
         } else {
             Toast.makeText(this, "Id not found! Please go back and try again", Toast.LENGTH_LONG).show();
+            onBackPressed();
+            return;
         }
+
+        context = ManageMyPageActivity.this;
+        activity = ManageMyPageActivity.this;
+        sessionPref = new SessionPref(context);
+        apiInterface = ApiClient.getClient().create(ApiInterface.class);
+
+        back_cv = findViewById(R.id.back_cv);
+        back_cv.setOnClickListener(v -> {
+            onBackPressed();
+        });
+
+        view_pager = findViewById(R.id.view_pager);
+        tab_layout = findViewById(R.id.tab_layout);
+        tab_layout.setupWithViewPager(view_pager);
     }
 }
