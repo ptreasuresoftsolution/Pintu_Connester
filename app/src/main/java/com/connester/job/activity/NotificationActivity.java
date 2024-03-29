e660b41a066e7babaa63c24bf0ec14bbf75d8926
imac <pintu033dholariya@gmail.com>
2024-03-29 11:03:55 +0530

    chatting progress

--- app/src/main/java/com/connester/job/activity/NotificationActivity.java
+++ app/src/main/java/com/connester/job/activity/NotificationActivity.java
@@ -1,16 +1,51 @@
 package com.connester.job.activity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.connester.job.R;
+import com.connester.job.RetrofitConnection.ApiClient;
+import com.connester.job.RetrofitConnection.ApiInterface;
+import com.connester.job.function.SessionPref;
+
+import java.util.HashMap;
 
 public class NotificationActivity extends AppCompatActivity {
+    SessionPref sessionPref;
+    Context context;
+    Activity activity;
+    LinearLayout main_ll;
+    ScrollView scrollView;
+    FrameLayout progressBar;
+    ApiInterface apiInterface;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+
+        context = this;
+        activity = this;
+        sessionPref = new SessionPref(context);
+        apiInterface = ApiClient.getClient().create(ApiInterface.class);
+
+        main_ll = findViewById(R.id.main_ll);
+        scrollView = findViewById(R.id.scrollView);
+        progressBar = findViewById(R.id.progressBar);
+
+        setData();
+    }
+
+    private void setData() {
+        HashMap hashMap = new HashMap();
+        hashMap.put("",sessionPref.getUserMasterId());
+        hashMap.put("",sessionPref.getApiKey());
+
     }
 }
